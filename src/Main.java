2b929cbf207ef6f64c91a019ed661769ca41ad24
Korinna Sinelnikova <103421831+Kori-si@users.noreply.github.com>
2023-04-12 13:32:29 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import Classes.Zoo;
+import GUI.UI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UI.run();
+
+    }
+}
